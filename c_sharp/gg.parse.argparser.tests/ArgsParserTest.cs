ac501d2ffc876e30aac0218059e3e2775541ee7d
pointlesspun <anotherpointlesspun@gmail.com>
2025-10-11 15:35:30 +0200

    adding more arg tests and fixes

--- c_sharp/gg.parse.argparser.tests/ArgsParserTest.cs
+++ c_sharp/gg.parse.argparser.tests/ArgsParserTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 
 using gg.parse.script;
+using gg.parse.script.parser;
 using gg.parse.script.pipeline;
 
 using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
@@ -49,14 +50,14 @@ namespace gg.parse.argparser.tests
             IsTrue(tokens);
             IsTrue(tokens.Count == 1);
             IsTrue(tokens.MatchLength == 2);
-            IsTrue(tokens[0].Rule.Name == "arg_option");
+            IsTrue(tokens[0].Rule.Name == "arg_key");
 
             IsTrue(syntaxTree);
             IsTrue(syntaxTree.Count == 1);
             IsTrue(syntaxTree.MatchLength == 1);
 
-            IsTrue(syntaxTree[0].Name == "optional_arg");
-            IsTrue(syntaxTree[0][0].Name == "arg_option");
+            IsTrue(syntaxTree[0].Name == "arg_option");
+            IsTrue(syntaxTree[0][0].Name == "arg_key");
 
             var optionText = tokens[syntaxTree[0][0].Start][0].GetText("-v");
 
@@ -73,14 +74,14 @@ namespace gg.parse.argparser.tests
             IsTrue(tokens);
             IsTrue(tokens.Count == 1);
             IsTrue(tokens.MatchLength == argText.Length);
-            IsTrue(tokens[0].Rule.Name == "arg_option");
+            IsTrue(tokens[0].Rule.Name == "arg_key");
 
             IsTrue(syntaxTree);
             IsTrue(syntaxTree.Count == 1);
             IsTrue(syntaxTree.MatchLength == 1);
 
-            IsTrue(syntaxTree[0].Name == "optional_arg");
-            IsTrue(syntaxTree[0][0].Name == "arg_option");
+            IsTrue(syntaxTree[0].Name == "arg_option");
+            IsTrue(syntaxTree[0][0].Name == "arg_key");
 
             var optionText = tokens[syntaxTree[0][0].Start][0].GetText(argText);
 
@@ -96,13 +97,13 @@ namespace gg.parse.argparser.tests
             var (tokens, syntaxTree) = builder.Parse(argText);
 
             IsTrue(tokens);
-            IsTrue(tokens[0].Name == "arg_option");
+            IsTrue(tokens[0].Name == "arg_key");
             IsTrue(tokens[1].Name == "kv_separator");
             IsTrue(tokens[2].Name == "arg_identifier");
 
             IsTrue(syntaxTree);
-            IsTrue(syntaxTree[0].Rule.Name == "optional_arg");
-            IsTrue(syntaxTree[0][0].Rule.Name == "arg_option");
+            IsTrue(syntaxTree[0].Rule.Name == "arg_option");
+            IsTrue(syntaxTree[0][0].Rule.Name == "arg_key");
             IsTrue(syntaxTree[0][1].Rule.Name == "arg_value");
 
             var valueText = syntaxTree[0][1].GetText(argText, tokens);
@@ -116,9 +117,8 @@ namespace gg.parse.argparser.tests
             var (tokens, syntaxTree) = builder.Parse("value");
 
             IsTrue(syntaxTree);
-            IsTrue(syntaxTree[0].Rule.Name == "required_arg");
-            IsTrue(syntaxTree[0][0].Rule.Name == "arg_value");
-            IsTrue(syntaxTree[0][0][0].Rule.Name == "arg_identifier");
+            IsTrue(syntaxTree[0].Rule.Name == "arg_value");
+            IsTrue(syntaxTree[0][0].Rule.Name == "arg_identifier");
 
             var valueText = syntaxTree[0].GetText("value", tokens);
             IsTrue(valueText == "value");
@@ -129,41 +129,99 @@ namespace gg.parse.argparser.tests
         {
             var builder = new ParserBuilder().FromFile(TokenFileName, GrammarFileName);
 
+            var argText = "-s=123 --input=c:\\some\\file.txt command";
+            var (tokens, syntaxTree) = builder.Parse(argText);
+
+            IsTrue(tokens);
+            IsTrue(tokens.Count == 10);
+
+            IsTrue(syntaxTree);
+            IsTrue(syntaxTree.Count == 3);
+            IsTrue(syntaxTree.MatchLength == 10);
+
+            IsTrue(syntaxTree[0].Name == "arg_option");
+            IsTrue(syntaxTree[0][0].Name == "arg_key");
+            IsTrue(syntaxTree[0][0].GetText(argText, tokens.Annotations) == "-s");
+            IsTrue(syntaxTree[0][1].Name == "arg_value");
+            IsTrue(syntaxTree[0][1][0].Name == "int");
+            IsTrue(syntaxTree[0][1][0].GetText(argText, tokens.Annotations) == "123");
+
+            IsTrue(syntaxTree[1].Name == "arg_option");
+            IsTrue(syntaxTree[1][0].Name == "arg_key");
+            IsTrue(syntaxTree[1][0].GetText(argText, tokens.Annotations) == "--input");
+            IsTrue(syntaxTree[1][1].Name == "arg_value");
+            IsTrue(syntaxTree[1][1][0].Name == "filename");
+            IsTrue(syntaxTree[1][1][0].GetText(argText, tokens.Annotations) == "c:\\some\\file.txt");
+
+            IsTrue(syntaxTree[2].Name == "arg_value");
+            IsTrue(syntaxTree[2][0].Name == "arg_identifier");
+            IsTrue(syntaxTree[2][0].GetText(argText, tokens.Annotations) == "command");
+        }
+
+        [TestMethod]
+        public void SetupMultipleOptionalAndRequired_Parse_ExpectMatch()
+        {
+            var builder = new ParserBuilder().FromFile(TokenFileName, GrammarFileName);
+
+            var argText = "-s=123 command1 command2 --next_option:-1.0 command3";
+            var (tokens, syntaxTree) = builder.Parse(argText);
+
+            IsTrue(syntaxTree);
+
+            IsTrue(syntaxTree[0].Name == "arg_option");
+            IsTrue(syntaxTree[0][0].GetText(argText, tokens.Annotations) == "-s");
+            IsTrue(syntaxTree[0][1].GetText(argText, tokens.Annotations) == "123");
+
+            IsTrue(syntaxTree[1].Name == "arg_value");
+            IsTrue(syntaxTree[1].GetText(argText, tokens.Annotations) == "command1");
+
+            IsTrue(syntaxTree[2].Name == "arg_value");
+            IsTrue(syntaxTree[2].GetText(argText, tokens.Annotations) == "command2");
+
+            IsTrue(syntaxTree[3].Name == "arg_option");
+            IsTrue(syntaxTree[3][0].GetText(argText, tokens.Annotations) == "--next_option");
+            IsTrue(syntaxTree[3][1].GetText(argText, tokens.Annotations) == "-1.0");
+
+            IsTrue(syntaxTree[4].Name == "arg_value");
+            IsTrue(syntaxTree[4].GetText(argText, tokens.Annotations) == "command3");
+        }
+
+        [TestMethod]
+        public void SetupUnknownToken_Parse_ExpectScriptException()
+        {
+            var builder = new ParserBuilder().FromFile(TokenFileName, GrammarFileName);
+            var argText = "|";
+
+
+            builder.LogHandler.Out = (level, message) => Debug.WriteLine(message);
+
             try
             {
-                var argText = "-s=123 --input=c:\\some\\file.txt command";
-                var (tokens, syntaxTree) = builder.Parse(argText);
-
-                IsTrue(tokens);
-                IsTrue(tokens.Count == 10);
-
-                IsTrue(syntaxTree);
-                IsTrue(syntaxTree.Count == 3);
-                IsTrue(syntaxTree.MatchLength == 10);
-
-                IsTrue(syntaxTree[0].Name == "optional_arg");
-                IsTrue(syntaxTree[0][0].Name == "arg_option");
-                IsTrue(syntaxTree[0][0].GetText(argText, tokens.Annotations) == "-s");
-                IsTrue(syntaxTree[0][1].Name == "arg_value");
-                IsTrue(syntaxTree[0][1][0].Name == "int");
-                IsTrue(syntaxTree[0][1][0].GetText(argText, tokens.Annotations) == "123");
-
-                IsTrue(syntaxTree[1].Name == "optional_arg");
-                IsTrue(syntaxTree[1][0].Name == "arg_option");
-                IsTrue(syntaxTree[1][0].GetText(argText, tokens.Annotations) == "--input");
-                IsTrue(syntaxTree[1][1].Name == "arg_value");
-                IsTrue(syntaxTree[1][1][0].Name == "filename");
-                IsTrue(syntaxTree[1][1][0].GetText(argText, tokens.Annotations) == "c:\\some\\file.txt");
-
-                IsTrue(syntaxTree[2].Name == "required_arg");
-                IsTrue(syntaxTree[2][0][0].Name == "arg_identifier");
-                IsTrue(syntaxTree[2][0][0].GetText(argText, tokens.Annotations) == "command");
-            }
-            catch (Exception ex)
-            {
+                builder.Parse(argText);
                 Fail();
+            }
+            catch (ScriptException)
+            {   
             }
         }
 
+        [TestMethod]
+        public void SetupUnknownGrammar_Parse_ExpectScriptException()
+        {
+            var builder = new ParserBuilder().FromFile(TokenFileName, GrammarFileName);
+            var argText = ":boo";
+
+
+            builder.LogHandler.Out = (level, message) => Debug.WriteLine(message);
+
+            try
+            {
+                var (tokens, syntaxTree) = builder.Parse(argText);
+                Fail();
+            }
+            catch (ScriptException)
+            {
+            }
+        }
     }
 }
