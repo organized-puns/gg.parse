0092231347ecd49e4ea1f995b48fd6680c4da489
pointlesspun <anotherpointlesspun@gmail.com>
2025-10-29 23:22:00 +0100

    Not removing the id as it's used by the grammar to identify tokens. Adding some documentation

--- c_sharp/gg.parse/IRule.cs
+++ c_sharp/gg.parse/IRule.cs
@@ -5,9 +5,14 @@ namespace gg.parse
 {
     public partial interface IRule 
     {
-        // xxx remove - name is the identifier
+        /// <summary>
+        /// Unique id, used to identify a rule in a rule graph. Also used as tokens in the grammar parsers.
+        /// </summary>
         int Id { get; set; }
 
+        /// <summary>
+        /// Unique, preferably somewhat human-readable, name of the rule. Used to identify a rule in a rule graph
+        /// </summary>
         string Name { get; init; }
         
         int Precedence { get; init; }
